6ff53583b721e1250f0c17db46c781d2393d5ed9
Sami Mäkinen <sami.makinen@nls.fi>
2019-08-08 16:15:30 +0300

    Add non-admin user mentioned in docs

--- app-resources/src/main/resources/flyway/example/V1_0_1__initial-users.sql
+++ app-resources/src/main/resources/flyway/example/V1_0_1__initial-users.sql
@@ -1,4 +1,4 @@
--- insert only admin user;
+-- insert admin user;
 INSERT INTO oskari_users(user_name, first_name, last_name, uuid) VALUES('admin', 'Oskari', 'Admin', 'asdf-asdf-asdf-asdf-asdf');
 
 INSERT INTO oskari_role_oskari_user(user_id, role_id) VALUES((SELECT id FROM oskari_users WHERE user_name = 'admin'), (SELECT id FROM oskari_roles WHERE name = 'User'));
@@ -6,6 +6,14 @@ INSERT INTO oskari_role_oskari_user(user_id, role_id) VALUES((SELECT id FROM osk
 
 -- use admin/oskari credentials for admin user (user_name in oskari_users must match login on oskari_jaas_users);
 INSERT INTO oskari_jaas_users(login, password) VALUES('admin', '$2a$10$sWFKpoLsaOZ1Jo09b7oOe.B6Ndw8jNvmriO4I2GEcRUl9xVJIn496');
+
+-- insert "normal" user;
+INSERT INTO oskari_users(user_name, first_name, last_name, uuid) VALUES('user', 'Oskari', 'Demo user', 'asdf-asdf-asdf-asdf-asdf');
+INSERT INTO oskari_role_oskari_user(user_id, role_id) VALUES((SELECT id FROM oskari_users WHERE user_name = 'user'), (SELECT id FROM oskari_roles WHERE name = 'User'));
+
+-- use user/user credentials for "normal" user (user_name in oskari_users must match login on oskari_jaas_users);
+INSERT INTO oskari_jaas_users(login, password) VALUES('user', '$2a$10$FUFDkmy9Fw9ieBHRSLeFkOfNTNDUNdCmPdWZxTYku9OXIRRQpivR2');
+
 
 -- For selenium tests
 INSERT INTO oskari_users(user_name, first_name, last_name, uuid) VALUES('testirobot', 'Test', 'Robot', 'beep-bee-boop-pip-doo-weep');
