9f8d58863a2994627e233af487f36f63f11fbd67
Sami Mäkinen <sami.makinen@nls.fi>
2019-08-13 13:22:10 +0300

    Use another uuid for normal user and not the same as admin

--- app-resources/src/main/resources/flyway/example/V1_0_1__initial-users.sql
+++ app-resources/src/main/resources/flyway/example/V1_0_1__initial-users.sql
@@ -8,7 +8,7 @@ INSERT INTO oskari_role_oskari_user(user_id, role_id) VALUES((SELECT id FROM osk
 INSERT INTO oskari_jaas_users(login, password) VALUES('admin', '$2a$10$sWFKpoLsaOZ1Jo09b7oOe.B6Ndw8jNvmriO4I2GEcRUl9xVJIn496');
 
 -- insert "normal" user;
-INSERT INTO oskari_users(user_name, first_name, last_name, uuid) VALUES('user', 'Oskari', 'Demo user', 'asdf-asdf-asdf-asdf-asdf');
+INSERT INTO oskari_users(user_name, first_name, last_name, uuid) VALUES('user', 'Oskari', 'Demo user', 'user-user-user-user-user');
 INSERT INTO oskari_role_oskari_user(user_id, role_id) VALUES((SELECT id FROM oskari_users WHERE user_name = 'user'), (SELECT id FROM oskari_roles WHERE name = 'User'));
 
 -- use user/user credentials for "normal" user (user_name in oskari_users must match login on oskari_jaas_users);
