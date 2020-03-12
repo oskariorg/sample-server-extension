1c487508028e55401d1302488b71c44adc5c07ec
okauppinen <olli.kauppinen@nls.fi>
2020-03-12 22:15:06 +0200

    add nasa dataprovider

--- app-resources/src/main/resources/flyway/example/V1_0_0__initial_dataproviders.sql
+++ app-resources/src/main/resources/flyway/example/V1_0_0__initial_dataproviders.sql
@@ -1,2 +1,3 @@
 insert into oskari_dataprovider(locale) values('{ "en":{ "name":"Oskari.org"} }');
+insert into oskari_dataprovider(locale) values('{"en":{"name":"NASA EOSDIS"}}');
 INSERT INTO oskari_maplayer_group (locale) values ('{"fi":{"name":"Taustakartat"}, "sv": { "name" : "Bakgrundskartor"},"en": { "name" : "Background maps"}}');
