b3de3a6e36bd0aed198880a1d8b550f3ad00e726
Sami Mäkinen <sami.makinen@nls.fi>
2020-08-25 14:56:02 +0300

    Restore Oskari.org as data provider

--- app-resources/src/main/resources/flyway/example/V1_0_0__initial_dataproviders.sql
+++ app-resources/src/main/resources/flyway/example/V1_0_0__initial_dataproviders.sql
@@ -1,4 +1,6 @@
-insert into oskari_dataprovider(locale) values('{"en":{"name":"NASA EOSDIS"}}');
-insert into oskari_dataprovider(locale) values('{"fi":{"name":"Helsingin kaupunki"}, "sv": { "name" : "Helsingfors stad"},"en": { "name" : "City of Helsinki"}}');
-INSERT INTO oskari_maplayer_group (locale) values ('{"fi":{"name":"Taustakartat"}, "sv": { "name" : "Bakgrundskartor"},"en": { "name" : "Background maps"}}');
-INSERT INTO oskari_maplayer_group (locale) values ('{"fi":{"name":"Rakennukset"}, "sv": { "name" : "Byggnader"},"en": { "name" : "Buildings"}}');
+
+INSERT INTO oskari_dataprovider (locale) VALUES ('{ "en":{ "name":"Oskari.org"} }');
+INSERT INTO oskari_dataprovider (locale) VALUES ('{"en":{"name":"NASA EOSDIS"}}');
+INSERT INTO oskari_dataprovider (locale) VALUES ('{"fi":{"name":"Helsingin kaupunki"}, "sv": { "name" : "Helsingfors stad"},"en": { "name" : "City of Helsinki"}}');
+INSERT INTO oskari_maplayer_group (locale) VALUES ('{"fi":{"name":"Taustakartat"}, "sv": { "name" : "Bakgrundskartor"},"en": { "name" : "Background maps"}}');
+INSERT INTO oskari_maplayer_group (locale) VALUES ('{"fi":{"name":"Rakennukset"}, "sv": { "name" : "Byggnader"},"en": { "name" : "Buildings"}}');
