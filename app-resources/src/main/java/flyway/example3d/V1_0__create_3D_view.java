72d0155a41fb035f0a3ec6cec39a84e79a837d99
Sami Mäkinen <sami.makinen@nls.fi>
2020-09-14 15:14:23 +0300

    Use explicit paths for json files

--- app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
+++ app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
@@ -12,13 +12,13 @@ public class V1_0__create_3D_view extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
          // insert layers based on json under /src/main/resources/json/layers/
-        LayerHelper.setupLayer("hki-3d-model.json");
-        LayerHelper.setupLayer("hki-3d-buildings.json");
-        LayerHelper.setupLayer("nasa-features.json");
-        LayerHelper.setupLayer("nasa-labels.json");
-        LayerHelper.setupLayer("nasa-water-mask.json");
+        LayerHelper.setupLayer("/json/layers/hki-3d-model.json");
+        LayerHelper.setupLayer("/json/layers/hki-3d-buildings.json");
+        LayerHelper.setupLayer("/json/layers/nasa-features.json");
+        LayerHelper.setupLayer("/json/layers/nasa-labels.json");
+        LayerHelper.setupLayer("/json/layers/nasa-water-mask.json");
 
         // add applications based on json under /src/main/resources/json/views/
-        AppSetupHelper.create(context.getConnection(), "geoportal-3d.json");
+        AppSetupHelper.create(context.getConnection(), "/json/apps/geoportal-3d.json");
     }
 }
--- app-resources/src/main/java/flyway/example3d/V1_2__setup_dimension_change.java
+++ app-resources/src/main/java/flyway/example3d/V1_2__setup_dimension_change.java
@@ -1,8 +1,6 @@
 package flyway.example3d;
 
 import fi.nls.oskari.domain.map.view.Bundle;
-import fi.nls.oskari.log.LogFactory;
-import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.util.JSONHelper;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
@@ -13,7 +11,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class V1_2__setup_dimension_change extends BaseJavaMigration {
-    private static final Logger LOG = LogFactory.getLogger(V1_2__setup_dimension_change.class);
     private static final String BUNDLE_NAME = "dimension-change";
     private static final String APPLICATION_3D_NAME = "geoportal-3d";
     private static final String APPLICATION_2D_NAME = "geoportal";
@@ -23,25 +20,20 @@ public class V1_2__setup_dimension_change extends BaseJavaMigration {
         // Set 3D view's uuid for 2D default and user appsetups
         List<Long> viewIds2d = AppSetupHelper.getSetupsForUserAndDefaultType(conn, APPLICATION_2D_NAME);
         String uuid3d = AppSetupHelper.getUuidForDefaultSetup(conn, APPLICATION_3D_NAME);
-        setBundleForViews(conn, viewIds2d, uuid3d);
+        AppSetupHelper.addOrUpdateBundleInApps(conn, generateDimensionChange(uuid3d), viewIds2d);
 
         // Set default 2D view's uuid for 3D default appsetup
         List<Long> viewIds3d = AppSetupHelper.getSetupsForUserAndDefaultType(conn, APPLICATION_3D_NAME);
         String uuid2d =  AppSetupHelper.getUuidForDefaultSetup( conn, APPLICATION_2D_NAME);
-        setBundleForViews(conn, viewIds3d, uuid2d);
+        AppSetupHelper.addOrUpdateBundleInApps(conn, generateDimensionChange(uuid2d), viewIds3d);
     }
-    private void setBundleForViews (Connection conn, List<Long> ids, String uuid) throws SQLException {
+
+    private Bundle generateDimensionChange(String uuid) {
+        Bundle bundle = new Bundle(BUNDLE_NAME);
         String conf = JSONHelper.getStringFromJSON(
                 JSONHelper.createJSONObject("uuid", uuid)
                 ,"{}");
-        for (Long id : ids) {
-            if (!AppSetupHelper.appContainsBundle(conn, id, BUNDLE_NAME)) {
-                AppSetupHelper.addBundleToApp(conn, id, BUNDLE_NAME);
-            }
-            Bundle bundle = AppSetupHelper.getAppBundle(conn, id, BUNDLE_NAME);
-            bundle.setConfig(conf);
-            AppSetupHelper.updateAppBundle(conn, id, bundle);
-        }
-        LOG.info("Set view uuid:", uuid, "to dimension change bundle config. Updated views:", ids);
+        bundle.setConfig(conf);
+        return bundle;
     }
 }
