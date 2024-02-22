cb03440b4dd70ac277a185c95247c9be371d2350
Pekka Helesuo <pekka.helesuo@siili.com>
2024-02-22 16:02:02 +0200

    add find by coordinates bundle to DEFAULT views

---
+++ app-resources/src/main/java/flyway/example/V1_1_4__add_findbycoordinates_bundle.java
@@ -0,0 +1,19 @@
+package flyway.example;
+
+import fi.nls.oskari.domain.map.view.Bundle;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.oskari.helpers.AppSetupHelper;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class V1_1_4__add_findbycoordinates_bundle extends BaseJavaMigration {
+    @Override
+    public void migrate(Context context) throws Exception {
+        Bundle findByCoordinatesBundle = new Bundle("findbycoordinates");
+        Connection connection = context.getConnection();
+        List<Long> appsetupIds = AppSetupHelper.getSetupsForType(connection,"DEFAULT");
+        AppSetupHelper.addOrUpdateBundleInApps(connection, findByCoordinatesBundle, appsetupIds);
+    }
+}
