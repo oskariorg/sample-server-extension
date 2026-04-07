b5ae781fad69124238d19d179f23f5d1ae2c9557
Sami Mäkinen <sami.makinen@nls.fi>
2026-04-07 14:57:12 +0300

    HKI has removed these, remove references

--- app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
+++ app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
@@ -12,7 +12,6 @@ public class V1_0__create_3D_view extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
          // insert layers based on json under /src/main/resources/json/layers/
-        LayerHelper.setupLayer("/json/layers/hki-3d-model.json");
         LayerHelper.setupLayer("/json/layers/nasa-features.json");
         LayerHelper.setupLayer("/json/layers/nasa-labels.json");
 
